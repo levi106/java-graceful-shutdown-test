d1852d9219c1b74c53acfcd851fafbf01e53f8cc
Hiroyuki Hasebe <16788901+levi106@users.noreply.github.com>
2022-09-29 16:08:12 +0900

    initial commit

---
+++ src/gracefulshutdowntest/src/main/java/com/example/gracefulshutdowntest/GracefulshutdowntestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gracefulshutdowntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GracefulshutdowntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GracefulshutdowntestApplication.class, args);
+	}
+
+}
---
+++ src/gracefulshutdowntest/src/main/java/com/example/gracefulshutdowntest/controller/SleepController.java
@@ -0,0 +1,37 @@
+package com.example.gracefulshutdowntest.controller;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequestMapping("sleep")
+public class SleepController {
+    private static final SimpleDateFormat DF = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+    @RequestMapping("/{millis}")
+    public Mono<String> get(@PathVariable Integer millis) {
+        log.info("sleep: {}", millis);
+        final StopWatch sw = new StopWatch();
+        log.info("Thread # {}: start {}", Thread.currentThread().getId(), DF.format(Date.from(Instant.now())));
+        sw.start();
+        return Mono.just(sw)
+            .delayElement(Duration.ofMillis(millis))
+            .log()
+            .map(x -> {
+                log.info("Thread # {}: map {}", Thread.currentThread().getId(), DF.format(Date.from(Instant.now())));
+                x.stop();
+                return Long.toString(x.getTotalTimeMillis());
+            });
+    }
+}
